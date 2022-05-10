7e88f1521a6638fad0c3419ad08d4aa0ca03e767
yoann-cathelain <yoanncathelain23@gmail.com>
2022-05-10 11:35:36 +0200

    ini branch perso

---
+++ ptut_accueil.php
@@ -0,0 +1,16 @@
+<?php
+    include_once('ptut_db_connexion.php');
+?>
+
+<!DOCTYPE HTML>
+    <html lang = "fr">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style_projet.css" />
+            <?php
+                include_once('ptut_navbar.php');
+            ?>
+            
+
+    </html>
+
+                
