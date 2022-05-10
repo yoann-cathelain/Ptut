777699554d619f0ab36be2cade4f0d119172e1ad
yoann-cathelain <yoanncathelain23@gmail.com>
2022-05-10 11:16:43 +0200

    reini branch yoann

--- ptut_user_deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('ptut_db_connexion.php');
-    session_start();
-    $_SESSION['username'] ="";
-    $url_retour = "ptut_accueil.php";
-    echo "Vous avez été déconnecté <a href='".$url_retour."'>Retour</a>";
-?>
